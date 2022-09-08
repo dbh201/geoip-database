20bf33cb756cf2fe50329f02d25c2d908ef85abf
dbh201 <47301809+dbh201@users.noreply.github.com>
2022-09-08 19:32:12 +0000

    switch to database-files branch before retrieving

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ FILES="GeoIP GeoIPv6 GeoIPCity GeoIPCityv6 GeoIPASNum GeoIPASNumv6"
 TODAY="$(date -I | sed -E "s/-//g")"
 
 cd $GITHUB_WORKSPACE ;
+git checkout database-files ;
 rm sha256sums.txt ;
 
 for FILE in $FILES ; do
@@ -14,6 +15,6 @@ done
 # upload to github
 git add . ;
 git config user.name "$GITHUB_ACTOR" ;
-git commit -m "update: $TODAY" ;
+git commit -m "automated commit: $TODAY" ;
 git tag -m "automated commit on $(date)" -a $TODAY HEAD ;
 git push ;
