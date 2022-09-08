52c22794b8aebf55fa89456949e7c432aa4f8347
dbh201 <47301809+dbh201@users.noreply.github.com>
2022-09-08 20:26:05 +0000

    fixed tags not being pushed; some cleanup

--- update.sh
+++ update.sh
@@ -4,17 +4,19 @@ FILES="GeoIP GeoIPv6 GeoIPCity GeoIPCityv6 GeoIPASNum GeoIPASNumv6"
 TODAY="$(date -I | sed -E "s/-//g")"
 
 cd $GITHUB_WORKSPACE ;
+git fetch --depth=1 ;
 git checkout database-files ;
 rm sha256sums.txt ;
 
 for FILE in $FILES ; do
 	curl $URL/$FILE.dat.gz -o $FILE.dat.gz ;
 	sha256sum $FILE.dat.gz >> sha256sums.txt ;
+	git add $FILE.dat.gz ;
 done
+git add sha256sums.txt ;
 
 # upload to github
-git add . ;
-git config user.name "$GITHUB_ACTOR" ;
+git config user.name "robot-$GITHUB_ACTOR" ;
 git commit -m "automated commit: $TODAY" ;
 git tag -m "automated commit on $(date)" -a $TODAY HEAD ;
-git push ;
+git push --tags ;
