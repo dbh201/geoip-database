bb624e54c165e58b8af580ecd28abe459fb93a83
dbh201 <47301809+dbh201@users.noreply.github.com>
2022-09-08 19:13:39 +0000

    update.sh and GitHub workflow

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+URL="https://mailfud.org/geoip-legacy"
+FILES="GeoIP GeoIPv6 GeoIPCity GeoIPCityv6 GeoIPASNum GeoIPASNumv6"
+TODAY="$(date -I | sed -E "s/-//g")"
+
+cd $GITHUB_WORKSPACE ;
+rm sha256sums.txt ;
+
+for FILE in $FILES ; do
+	curl $URL/$FILE.dat.gz -o $FILE.dat.gz ;
+	sha256sum $FILE.dat.gz >> sha256sums.txt ;
+done
+
+# upload to github
+git add . ;
+git config user.name "$GITHUB_ACTOR" ;
+git commit -m "update: $TODAY" ;
+git tag -m "automated commit on $(date)" -a $TODAY HEAD ;
+git push ;
