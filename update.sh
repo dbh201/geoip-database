5972d42ac758cd3610101b9613cb19715c162e14
dbh201 <47301809+dbh201@users.noreply.github.com>
2022-09-11 12:28:36 +0000

    final touches; remove unnecessary branch

--- update.sh
+++ update.sh
@@ -4,9 +4,6 @@ FILES="GeoIP GeoIPv6 GeoIPCity GeoIPCityv6 GeoIPASNum GeoIPASNumv6"
 TODAY="$(date -I | sed -E "s/-//g")"
 
 cd $GITHUB_WORKSPACE ;
-git fetch --depth=1 ;
-git checkout database-files ;
-rm sha256sums.txt ;
 
 for FILE in $FILES ; do
 	curl $URL/$FILE.dat.gz -o $FILE.dat.gz ;
@@ -17,6 +14,6 @@ git add sha256sums.txt ;
 
 # upload to github
 git config user.name "robot-$GITHUB_ACTOR" ;
-git commit -m "automated commit: $TODAY" ;
+git commit -m "automated commit on $(date)" ;
 git tag -m "automated commit on $(date)" -a $TODAY HEAD ;
 git push --tags ;
